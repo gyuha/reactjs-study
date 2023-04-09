4eeaeb4671b0370393ffb6ea6c891764f98301ae
gyuha <nicegyuha@gmail.com>
2023-04-09 23:55:16 +0900

    theme는 포기 -_-;

---
+++ imdb-clone/components/providers.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+
+
+const Providers = ({ children }) => {
+  return (
+    <ThemeProvider enableSystem={true} attrbbibute="class">
+      <div className="dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-300 min-h-screen select-none">
+        {children}
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default Providers;
